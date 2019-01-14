0682b77a00e01e230adaad2e7b00a150fd9a163a
Samet Doner <smet.doner@gmail.com>
2019-01-14 20:19:47 +0300

    beginning

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
