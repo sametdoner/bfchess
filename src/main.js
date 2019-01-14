dca837d6b5ec1dadd092aab1c75e99fd55f219de
Samet Doner <smet.doner@gmail.com>
2019-01-14 22:38:40 +0300

    packages

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BootstrapVue from 'bootstrap-vue';
+import JQuery from 'jquery';
 
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+global.jQuery = jQuery;
 Vue.config.productionTip = false;
+
+// global.bootstrap = bootstrap;
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
-
+    <div id="board" style="width=400px"></div>
+    <div id="example">
+  <button v-on:click="greet">Greet</button>
+</div>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '@/components/Navbar.vue';
 
+// import JQuery from 'jquery';
+// let $ = JQuery;
+
 export default {
   name: 'home',
+  methods: {
+
+  },
   components: {
     Navbar,
   },
