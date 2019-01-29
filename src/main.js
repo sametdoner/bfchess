1b81e6abf732e198a408cc0ad96c3235e4987853
Samet Doner <smet.doner@gmail.com>
2019-01-29 23:57:30 +0300

    legal moves

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
-// import jQuery from 'jquery';
+import jQuery from 'jquery';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Chess from '../node_modules/chess.js';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-// global.jQuery = jQuery;
+global.jQuery = jQuery;
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,100 @@
 <template>
   <div class="home">
     <div id="board" style="width: 450px; margin: 20px auto"></div>
-    <input type="button" @click.prevent="startgame" value="Start" />
-    <!-- <input type="button" id="clearBtn" value="Clear" /> -->
+    <!-- <input type="button" @click.prevent="startgame" value="Start" /> -->
+    <!-- <input type="button" id="setStartBtn" value="Start Position" /> -->
   </div>
 
 
 </template>
 
 <script>
-// @ is an alias to /src
-// import Navbar from '@/components/Navbar.vue';
-// import 'chessboardjs';
+import jQuery from 'jquery';
+import Chess from 'chess.js';
 
-// import jQuery from 'jquery';
-// const $ = jQuery;
+const $ = jQuery;
 
 export default {
   name: 'home',
   mounted() {
-    this.board = window.ChessBoard('board', {
+    let board = new Chess();
+    const game = new Chess();
+    function removeGreySquares() {
+      $('#board .square-55d63').css('background', '');
+    }
+
+    function greySquare(square) {
+      const squareEl = $('#board .square-' + square);
+
+      var background = '#a9a9a9';
+      if (squareEl.hasClass('black-3c85d') === true) {
+        var background = '#696969';
+      }
+      squareEl.css('background', background);
+    }
+    function onDragStart(source, piece) {
+    // do not pick up pieces if the game is over
+    // or if it's not that side's turn
+      if (game.game_over() === true || (game.turn() === 'w' && piece.search(/^b/) !== -1)
+        || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+      }
+      return true;
+    }
+
+    function onDrop(source, target) {
+      removeGreySquares();
+
+      // see if the move is legal
+      const move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q', // NOTE: always promote to a queen for example simplicity
+      });
+      // illegal move
+      if (move === null) {
+        return 'snapback';
+      }
+      return true;
+    }
+
+    function onMouseoverSquare(square) {
+    // get list of possible moves for this square
+      const moves = game.moves({
+        square,
+        verbose: true,
+      });
+
+      // exit if there are no moves available for this square
+      if (moves.length === 0) return;
+
+      // highlight the square they moused over
+      greySquare(square);
+
+      // highlight the possible squares for this piece
+      for (let i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+      }
+    }
+
+    function onMouseoutSquare() {
+      removeGreySquares();
+    }
+
+    function onSnapEnd() {
+      board.position(game.fen());
+    }
+
+    const cfg = {
       draggable: true,
-      dropOffBoard: 'trash',
-      sparePieces: true,
-    });
-    // $('#startBtn').on('click', );
-    // $('#clearBtn').on('click', board.clear);
-  },
-  methods: {
-    startgame() {
-      console.log('hop');
-      console.log(this.board);
-      this.board.start();
-      // const board = Chessboard('board');
-      // $('#startBtn').on('click', board.start);
-      // $('#clearBtn').on('click', board.clear);
-    },
+      position: 'start',
+      onDragStart,
+      onDrop,
+      onMouseoutSquare,
+      onMouseoverSquare,
+      onSnapEnd,
+    };
+    board = window.ChessBoard('board', cfg);
   },
   data() {
     return {
